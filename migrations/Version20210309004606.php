2549b8e56d5f9f4f59b40503969b68592b7990dd
Branyer Vergara <Branyervergarac@gmail.com>
2021-03-08 21:22:57 -0400

    corrigiendo bug en el return del controlador, agregando ruta /admin/services y /admin/services/internet

--- migrations/Version20210309004606.php
+++ migrations/Version20210309004606.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210308181349 extends AbstractMigration
+final class Version20210309004606 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,14 +20,20 @@ final class Version20210308181349 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE channel (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE internet (id INT AUTO_INCREMENT NOT NULL, speed INT NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE program (id INT AUTO_INCREMENT NOT NULL, channel_id_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, week_day DATE NOT NULL, hour TIME NOT NULL, INDEX IDX_92ED7784C86596CF (channel_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE telephony (id INT AUTO_INCREMENT NOT NULL, minutes INT NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED7784C86596CF FOREIGN KEY (channel_id_id) REFERENCES channel (id)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED7784C86596CF');
+        $this->addSql('DROP TABLE channel');
         $this->addSql('DROP TABLE internet');
+        $this->addSql('DROP TABLE program');
         $this->addSql('DROP TABLE telephony');
     }
 }
